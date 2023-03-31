55228fe52dd84f338b529b99ffefd56f1a914266
gitachavan <126804942+gitachavan@users.noreply.github.com>
2023-03-31 09:33:01 +0530

    Add files via upload

---
+++ W7.java
@@ -0,0 +1,15 @@
+class W7
+{
+	public static void main(String[] args) 
+	{
+	 
+	 long l=40;
+	 float f='L';
+	 double d='L';
+
+        
+        System.out.println(l);
+        System.out.println(f);        
+        System.out.println(d);
+	}
+}
